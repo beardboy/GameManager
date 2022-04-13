20b476039a97c6460d3bbc261f1391876357dc90
Thaddeus Beck <beardboy@gmail.com>
2022-04-13 15:53:57 -0600

    Removing NewPage

--- GameManager.RC/Views/ViewExtensions.cs
+++ GameManager.RC/Views/ViewExtensions.cs
@@ -7,7 +7,6 @@
             builder.Services.AddTransient<HomePage>();
             builder.Services.AddTransient<GamePage>();
             builder.Services.AddTransient<MainPage>();
-            builder.Services.AddTransient<NewPage>();
 
             return builder;
         }
